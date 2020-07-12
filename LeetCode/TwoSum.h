cbc65e932624edfd3fb22c471964a86ec428aa5d
corgistarer <wzhangshanghai@163.com>
2020-07-12 22:43:15 +0800

    “TwoSum add hashmap solution”

--- LeetCode/TwoSum.h
+++ LeetCode/TwoSum.h
@@ -1,9 +1,13 @@
 #include<vector>
+#include<unordered_map>
 using namespace std;
 
 class TwoSum {
 public:
 	vector<int> twoSum(vector<int>& nums, int target) {
+		// 暴力法
+		// 时间复杂度: O(n^2); 
+		// 空间复杂度: O(1)
 		vector<int> index;
 		for (int i = 0; i < nums.size(); i++) {
 			for (int j = 1; j < nums.size(); j++) {
@@ -17,6 +21,27 @@ public:
 				}
 			}
 		}
+		return index;
+	}
+
+	vector<int> twoSum2(vector<int>& nums, int target) {
+		// 哈希法
+		// 时间复杂度: O(nlogn); 
+		//空间复杂度: O(n);
+		//参考：https://leetcode-cn.com/problems/two-sum/solution/san-chong-fang-fa-bao-li-shuang-zhi-zhen-ha-xi-san/
+		vector<int> index;
+		unordered_map<int, int>hashmap;
+		for (int i = 0; i < nums.size(); i++) 
+		{
+				if (hashmap[target-nums[i]] && hashmap[target-nums[i] != i+1])
+				{
+					index.push_back(i);
+					index.push_back(hashmap[target - nums[i]]-1);
+					return index;
+				}
+				hashmap[nums[i]] = i + 1;
+		}
+
 		return index;
 	}
 };
