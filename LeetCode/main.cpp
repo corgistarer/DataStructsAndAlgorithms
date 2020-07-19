ff6a55d58dd0f6e635c658c2eb9de58aecf3a4ae
corgistarer <wzhangshanghai@163.com>
2020-07-19 13:52:21 +0800

    add unit test

--- LeetCode/main.cpp
+++ LeetCode/main.cpp
@@ -2,13 +2,24 @@
 #include "TwoSum.h"
 
 
+// No.1 两数之和
+void test_TwoSum(vector<int> &nums, int target);
+
 int main()
 {
+	// test TwoSum
 	vector<int> nums = { 2,5,7,11, 100, 200, 300};
 	int target = 302;
+	test_TwoSum(nums, target);
+	return 0;
+}
+
+
+// No.1 两数之和
+void test_TwoSum(vector<int> &nums, int target) {
 	TwoSum twosum;
 	vector<int> index = twosum.twoSum2(nums, target);
-    std::cout << "TwoSum Test:\n";
+	std::cout << "TwoSum Test:\n";
 	if (0 != index.size())
 	{
 		for (int i = 0; i < index.size(); i++)
@@ -16,8 +27,8 @@ int main()
 			std::cout << index[i] << " ";
 		}
 	}
-	else 
+	else
 	{
-		std::cout <<" ";
+		std::cout << " ";
 	}
 }
